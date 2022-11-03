89fce5ea6c24898bfd7294fe9ea888854732740a
MerchantKhalid88 <mohammadkhr88@gmail;com>
2022-11-03 21:54:12 +0100

    layout set

---
+++ src/Router/Routes/Routes.js
@@ -0,0 +1,18 @@
+import Main from "../../Layout/Main/Main";
+import Home from "../../Pages/Home/Home";
+
+const { createBrowserRouter } = require("react-router-dom");
+
+const router= createBrowserRouter([
+    {
+        path:'/',
+        element:<Main />,
+        children:[
+            {
+                path:'/',
+                element:<Home />,
+            }
+        ]
+    }
+])
+export default router;
